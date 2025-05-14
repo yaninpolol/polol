35481ff8fedfd4e140511bd6f67fbdc1fbbe212f
yaninpolol <yaninpolol@gmail.com>
2025-05-14 19:12:52 +0700

    Update bojo23.py

--- bojo23.py
+++ bojo23.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://0238fc3249582cff57dd__cr.mx:ff4062bfa3b7eb74@gw.dataimpulse.com:10023',
-        'https': 'https://0238fc3249582cff57dd__cr.mx:ff4062bfa3b7eb74@gw.dataimpulse.com:10023',
+        'http': 'http://7f80327c578b86068874__cr.mx:13e3de6667a042d5@gw.dataimpulse.com:10023',
+        'https': 'https://7f80327c578b86068874__cr.mx:13e3de6667a042d5@gw.dataimpulse.com:10023',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
