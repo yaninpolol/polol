61eb65a251668fada0dddafae49d2edb428af9a3
yaninpolol <yaninpolol@gmail.com>
2025-05-21 05:22:17 +0700

    Update bojo21.py

--- bojo21.py
+++ bojo21.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://c548ddb9a53d4c8d0752__cr.ar:101b5d49ffd54635@gw.dataimpulse.com:10021',
-        'https': 'https://c548ddb9a53d4c8d0752__cr.ar:101b5d49ffd54635@gw.dataimpulse.com:10021',
+        'http': 'http://e1c4f1992994a9b8fa46__cr.ar:101b5d49ffd54635@gw.dataimpulse.com:10021',
+        'https': 'https://e1c4f1992994a9b8fa46__cr.ar:101b5d49ffd54635@gw.dataimpulse.com:10021',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
