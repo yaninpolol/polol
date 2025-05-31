adef288f504daab271ca04414d9e3ae71cd7e2fb
yaninpolol <yaninpolol@gmail.com>
2025-05-31 13:44:50 +0700

    Update bojo22.py

--- bojo22.py
+++ bojo22.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/acb182bf-d049-4d8d-8de1-e27901ea1b8c")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/1ad76672-9818-4e33-8fce-87944cbe2c47")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
