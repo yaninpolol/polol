75d7b31aa4390063499908712d2522e4930b300a
yaninpolol <yaninpolol@gmail.com>
2025-06-05 14:38:26 +0700

    Update bojo22.py

--- bojo22.py
+++ bojo22.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10022',
-        'https': 'https://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10022',
+        'http': 'http://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10122',
+        'https': 'https://1dbaa052cf65bb3db213__cr.br:6dc234a7f64de12d@gw.dataimpulse.com:10122',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
