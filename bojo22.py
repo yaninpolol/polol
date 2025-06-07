dbf84a10345599172497343f23ba15e63af9ab47
yaninpolol <yaninpolol@gmail.com>
2025-06-07 13:02:56 +0700

    Update bojo22.py

--- bojo22.py
+++ bojo22.py
@@ -47,7 +47,7 @@ chrome_options.add_argument("--disable-blink-features=AutomationControlled") # M
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/e1a8f866-9e60-423e-8dbb-86fb30da8f7d")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/1102ee22-7d85-4e11-ac12-67f5007b0a22")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
